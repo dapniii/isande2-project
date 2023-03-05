3eece1b41183ae8a93a5501d707a17121582e93f
alymsantos13 <alyssa_marie_santos@dlsu.edu.ph>
2023-03-05 13:39:50 +0800

    header nav included

--- pages/fuel/index.js
+++ pages/fuel/index.js
@@ -33,5 +33,23 @@ export default function FuelPage() {
     );
   }
 
-  
+  return (
+    <>
+      <Grid minH="100vh" templateColumns={"1fr 7fr"} templateRows={"0fr 1fr"}>
+        <GridItem colStart={1} rowSpan={2} bg={"#222222"}>
+          <Navbar user={user} />
+        </GridItem>
+
+        <GridItem colStart={2} top={0} position={"sticky"} zIndex={2}>
+          <Header
+            breadcrumb={headerBreadcrumbs()}
+            main={headerMain()}
+            withShadow={true}
+          />
+        </GridItem>
+
+        {/* ADD GRAPH AND TABLE */}
+      </Grid>
+    </>
+  );
 }
