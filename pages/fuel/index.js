8a2174c278a3596752cbe40e54be8c46908501af
alymsantos13 <alyssa_marie_santos@dlsu.edu.ph>
2023-03-05 13:38:31 +0800

    fixing initial fuel page parts: Headers

--- pages/fuel/index.js
+++ pages/fuel/index.js
@@ -4,4 +4,34 @@ import { AddButton } from "@/components/buttons";
 import { useRouter } from "next/router";
 import { Grid, GridItem, Flex, Text } from "@chakra-ui/react";
 
+//add param {data}
+export default function FuelPage() {
+  const router = useRouter();
 
+  //Temp
+  const user = {
+    firstName: "FirstName",
+    role: "Admin",
+  };
+
+  function addFuelEntry() {
+    router.push("/fuel/addFuel");
+  }
+
+  function headerBreadcrumbs() {
+    return <></>;
+  }
+
+  function headerMain() {
+    return (
+      <Flex alignItems={"center"} justifyContent={"space-between"}>
+        <Text fontSize={"3xl"} fontWeight={"bold"}>
+          Fuel
+        </Text>
+        <AddButton title={"Add Fuel Entry"} clickFunction={addFuelEntry} />
+      </Flex>
+    );
+  }
+
+  
+}
